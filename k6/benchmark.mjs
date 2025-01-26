e7740c7ad41517d74f27a1276363a6edacd5bb83
Brandon Ros <brandonros1@gmail.com>
2025-01-26 11:46:00 -0500

    100tps

--- k6/benchmark.mjs
+++ k6/benchmark.mjs
@@ -2,13 +2,13 @@ import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 import http from 'k6/http';
 import { check } from 'k6';
 
-//const URL = 'https://pdf-generator.debian-k3s/api/rpc';
-const URL = 'http://localhost:3000/api/rpc';
+const URL = 'https://pdf-generator.debian-k3s/api/rpc';
+//const URL = 'http://localhost:3000/api/rpc';
 const TEST_PAGE = open('./test-page.txt');
 
 export const options = {
-  vus: 40, // virtual users
-  rps: 40, // requests per second
+  vus: 100, // virtual users
+  rps: 100, // requests per second
   duration: '300s',
 };
 
@@ -30,19 +30,19 @@ export default function () {
       try {
         body = JSON.parse(r.body)
       } catch (err) {
-        console.error(err.message)
+        console.error(`failed to JSON.parse: ${err.message}`)
         return false
       }
       if (body.error !== undefined) {
-        console.error(body.error)
+        console.error(`body.error !== undefined: ${JSON.stringify(body)}`)
         return false
       }
       if (body.result === undefined) {
-        console.error(body)
+        console.error(`body.result === undefined: ${JSON.stringify(body)}`)
         return false
       }
       if (typeof body.result !== 'string') {
-        console.error(body)
+        console.error(`body.result !== string: ${JSON.stringify(body)}`)
         return false
       }
       return true
